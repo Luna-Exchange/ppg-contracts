02328f1bd5333a42bdefa8908f09b728a1266163
Oliver Ryall <oliryall0404@gmail.com>
2023-03-07 12:57:51 +0000

    add: test ipfs uri

--- script/Dreambox.s.sol
+++ script/Dreambox.s.sol
@@ -7,7 +7,7 @@ import "../src/Dreambox.sol";
 contract DeployDreambox is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        string memory uri = "https://test-uri";
+        string memory uri = "ipfs://bafybeicbdypzqaz2y7gh6v526ojb2y3ckyw6tm3lcgiz365pcg6evuele4/ppg.dreambox/{id}.json";
         uint256[] memory amounts = new uint256[](1);
         amounts[0] = 300;
         address tokenReceiver = 0x3203617C22D58652Bbc12B2F6BD5566c365ea0d4;
