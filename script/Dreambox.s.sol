6ded7251c2b30bd7bbee827fc369b5be830acb98
Oliver Ryall <oliryall0404@gmail.com>
2023-03-10 07:14:19 +0000

    refactor

--- script/Dreambox.s.sol
+++ script/Dreambox.s.sol
@@ -10,7 +10,7 @@ contract DeployDreambox is Script {
         string memory uri = "ipfs://bafybeido32gbifgprl2fcx43u6ytvwdbc5pne5qmwt65wpdtxvqrvoiriu/ppg.dreambox/{id}.json";
         uint256[] memory amounts = new uint256[](1);
         amounts[0] = 200;
-        address tokenReceiver = 0x3203617C22D58652Bbc12B2F6BD5566c365ea0d4;
+        address tokenReceiver = 0x3203617C22D58652Bbc12B2F6BD5566c365ea0d4; // Gnosis-Safe
         // -------- Constructor ARGS --------
 
         vm.startBroadcast(deployerPrivateKey);
