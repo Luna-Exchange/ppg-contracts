a0cd5f4c0f1d19ea95f8c8f0450c07c5c692737d
lifeisgooddev <lifeisgooddev@gmail.com>
2023-06-28 09:52:37 -0700

    update deploy script

--- script/DigitalTwins.s.sol
+++ script/DigitalTwins.s.sol
@@ -7,14 +7,15 @@ import "../src/DigitalTwins.sol";
 contract DeployDigitalTwins is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address relayer = 0xeD3eB6b8d6440029908a865B888EeD8c63ad468b; // Gnosis-Safe
+        address relayer = 0xdA179bd337b0828Ed874A32196de3f4b00E1D446; // Gnosis-Safe
+        address payable feeRecipient = payable(0x8D6A3B0541d73e77A6ba0C7440054a8997457FE2);
         // -------- Constructor ARGS --------
 
         vm.startBroadcast(deployerPrivateKey);
 
-        DigitalTwins digitalTwins = new DigitalTwins("", relayer);
+        DigitalTwins digitalTwins = new DigitalTwins("ipfs://QmeuhcgHXsesZxdiSPdsag6XtsmNGWfDAke76SY4krkdcB/", relayer, feeRecipient);
         digitalTwins.setMintActive(true);
-        digitalTwins.setNftPrice(0.01 ether);
+        digitalTwins.setNftPrice(78.95 ether);
         vm.stopBroadcast();
     }
 }
--- src/DigitalTwins.sol
+++ src/DigitalTwins.sol
@@ -26,6 +26,9 @@ contract DigitalTwins is ERC1155, Ownable {
     // Emitted when relayer is set.
     event SetRelayer(address relayer);
 
+    // Emitted when fee recipient is set.
+    event SetFeeRecipient(address feeRecipient);
+
     // Mapping of account addresses that have already minted a dreambox.
     mapping(uint256 => bool) public claimed;
 
@@ -38,6 +41,9 @@ contract DigitalTwins is ERC1155, Ownable {
     // The Relayer address
     address public relayer;
 
+    // The Fee Recipient address
+    address payable public feeRecipient;
+
     /**
      * @dev Constructs a new Dreambox contract.
      * @param _uri The URI for the token metadata.
@@ -45,9 +51,11 @@ contract DigitalTwins is ERC1155, Ownable {
      */
     constructor(
         string memory _uri,
-        address _relayer
+        address _relayer,
+        address payable _feeRecipient
     ) ERC1155(_uri) {
         relayer = _relayer;
+        feeRecipient = _feeRecipient;
     }
 
     modifier onlyRelayer {
@@ -60,7 +68,7 @@ contract DigitalTwins is ERC1155, Ownable {
      * @return The URI string.
      */
     function uri(uint256 tokenId) public view override returns (string memory) {
-        return string(abi.encodePacked(super.uri(tokenId), tokenId));
+        return string(abi.encodePacked(super.uri(tokenId), _toString(tokenId)));
     }
     
     /**
@@ -104,6 +112,16 @@ contract DigitalTwins is ERC1155, Ownable {
     }
     
     /**
+     * @dev Sets the address of the fee recipient.
+     * Only the contract owner can call this function.
+     * @param _feeRecipient The new relayer address.
+     */
+    function setFeeRecipient(address payable _feeRecipient) external onlyOwner {
+        feeRecipient = _feeRecipient;
+        emit SetFeeRecipient(_feeRecipient);
+    }
+
+    /**
      * @dev Mints NFTs and sends them to the recipient.
      * @param recipient The address of the NFT recipient.
      * @param tokenIds The token IDs of the NFT.
@@ -140,5 +158,50 @@ contract DigitalTwins is ERC1155, Ownable {
         if(msg.value != nftPrice * amount) revert NotEnoughMoneyToBuyNft();
 
         _mint(recipient, tokenId, amount, "");
+
+        (bool success, ) = feeRecipient.call{value: msg.value}("");
+        require(success, "Failed to withdraw");
+    }
+
+    /**
+     * @dev Converts a uint256 to its ASCII string decimal representation.
+     */
+    function _toString(uint256 value) internal pure virtual returns (string memory str) {
+        assembly {
+            // The maximum value of a uint256 contains 78 digits (1 byte per digit), but
+            // we allocate 0xa0 bytes to keep the free memory pointer 32-byte word aligned.
+            // We will need 1 word for the trailing zeros padding, 1 word for the length,
+            // and 3 words for a maximum of 78 digits. Total: 5 * 0x20 = 0xa0.
+            let m := add(mload(0x40), 0xa0)
+            // Update the free memory pointer to allocate.
+            mstore(0x40, m)
+            // Assign the `str` to the end.
+            str := sub(m, 0x20)
+            // Zeroize the slot after the string.
+            mstore(str, 0)
+
+            // Cache the end of the memory to calculate the length later.
+            let end := str
+
+            // We write the string from rightmost digit to leftmost digit.
+            // The following is essentially a do-while loop that also handles the zero case.
+            // prettier-ignore
+            for { let temp := value } 1 {} {
+                str := sub(str, 1)
+                // Write the character to the pointer.
+                // The ASCII index of the '0' character is 48.
+                mstore8(str, add(48, mod(temp, 10)))
+                // Keep dividing `temp` until zero.
+                temp := div(temp, 10)
+                // prettier-ignore
+                if iszero(temp) { break }
+            }
+
+            let length := sub(end, str)
+            // Move the pointer 32 bytes leftwards to make room for the length.
+            str := sub(str, 0x20)
+            // Store the length.
+            mstore(str, length)
+        }
     }
 }
